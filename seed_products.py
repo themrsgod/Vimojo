3a68d1053fd9d17db8f470a5c5a8ec5d27d06429
moses <kishea.dev@gmail.com>
2023-08-31 00:20:33 +0300

    product details seeded

--- seed_products.py
+++ seed_products.py
@@ -11,7 +11,10 @@ def seed_products(count):
     for _ in range(count):
         name = f'Product {random.randint(1, 100)}'
         price = round(random.uniform(10, 1000), 2)
-        Product.objects.create(name=name, price=price)
+        description = f'This is a description for {name}'
+        image_path = './images/image-4.jpg'  # Replace with actual image path
+        Product.objects.create(name=name, price=price, description=description, image=image_path)
+
 
 if __name__ == '__main__':
     product_count = 20  # Number of products to create
